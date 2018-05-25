8e75ee53641d257c514e7a99774caff905fc4945
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-25 14:01:13 -0500

    sass installed

---
+++ src/components/table.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Table extends React.Component {
+    render() {
+     return <div class='table'></div>
+     
+    }
+} 
+
+export default Table;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import Table from './components/table'
 import registerServiceWorker from './registerServiceWorker';
+import './styles/css/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+render(<Table />, document.querySelector('#main'));
 registerServiceWorker();
