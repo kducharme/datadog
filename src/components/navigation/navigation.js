fb3ffdfef6a6669545f14b7cc26ebeec18d422a9
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-26 20:17:46 -0500

    structure added

---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav className="mainNavigation shoNuff" id="myId">
+                <Link to="/">Home</Link>
+                <Link to="/projects">List of Projects</Link>
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import Table from './components/table'
 import registerServiceWorker from './registerServiceWorker';
-import './styles/css/index.css';
 
 
 render(<Table />, document.querySelector('#main'));
