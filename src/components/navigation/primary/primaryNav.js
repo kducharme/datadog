f38ae2a94765a97d594dae89721734ac6830ed00
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-26 21:43:29 -0500

    components + sass working

---
+++ src/components/navigation/primary/primaryNav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class PrimaryNavigation extends React.Component {
+    render() {
+     return <div className='primaryNav'>hey</div>
+    }
+} 
+
+export default PrimaryNavigation;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import { render } from 'react-dom';
-import Table from './components/table/table'
-import Navigation from './components/navigation/nav'
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 
+// Page components
+import Table from './components/table/table'
+import PrimaryNavigation from './components/navigation/primary/primaryNav';
+
 ReactDOM.render(
     <Router>
-        <div>
+        <div className='application'>
+            <PrimaryNavigation />
             <Table />
         </div>
     </Router>
