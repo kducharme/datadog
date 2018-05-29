0f777576dd52b4ea9617154101c0e2d50ed06c2b
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-29 09:29:57 -0400

    page nav and search rendering

--- src/components/table/actions/actions.js
+++ src/components/table/actions/actions.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 class Actions extends React.Component {
     render() {
-     return <div className='actions'>table actions</div>
+        return (
+            <div className='actions'>
+                <input placeholder='Search monitors' className='actions__search'></input>
+                <p className='actions__results'>Showing 1-25 of 572 monitors<span>(0 selected)</span></p>
+
+
+            </div>
+        )
     }
-} 
+}
 
 export default Actions;
