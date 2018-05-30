33bf870f68ef465088bd97c8f49c75d10ad55812
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-30 15:13:52 -0500

    routing working

--- src/components/navigation/page/pageNav.js
+++ src/components/navigation/page/pageNav.js
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 
 class PageNavigation extends React.Component {
 
+
     render() {
         return (
             <div className='pageNav'>
                 <div className="pageNav__links">
-                <Link className='pageNav__link active'to="/">Monitors</Link>
-                <Link className='pageNav__link'to="/downtime">Downtime</Link>
+                    <Link className='pageNav__links--link active' to="/">Monitors</Link>
+                    <Link className='pageNav__links--link' to="/downtime">Downtime</Link>
                 </div>
                 <div className="pageNav__add">
+                    <button className='pageNav__add--button'>Add monitor +</button>
                 </div>
             </div>
         )
