5c3a26813ba111e7033eb430434371528da4eabc
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-30 16:35:03 -0500

    nav changing state on page change

--- src/components/navigation/page/pageNav.js
+++ src/components/navigation/page/pageNav.js
@@ -3,14 +3,27 @@ import PageRouter from '../../../router'
 import { Link } from "react-router-dom";
 
 class PageNavigation extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            active: 'Monitor'
+        }
+    }
 
+    isActive = (...value) => {
+        console.log('hi')
+        return value === this.props.active ? 'active' : ''
+    }
 
     render() {
+        const activeStyle = 'active';
+
         return (
             <div className='pageNav'>
                 <div className="pageNav__links">
-                    <Link className='pageNav__links--link active' to="/">Monitors</Link>
-                    <Link className='pageNav__links--link' to="/downtime">Downtime</Link>
+                    <Link className={`pageNav__links--link ${this.state.active === navItem ? activeStyle : {}}`} to="/">Monitors</Link>
+                    <Link className={`pageNav__links--link ${this.isActive()}`} to="/downtime">Downtime</Link>
                 </div>
                 <div className="pageNav__add">
                     <button className='pageNav__add--button'>Add monitor +</button>
