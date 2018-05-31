ef054dd33f59c5ef681cb9fe0a2818bc36072be3
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-31 15:54:38 -0500

    table status state working

--- src/components/table/header/tableHeader.js
+++ src/components/table/header/tableHeader.js
@@ -9,10 +9,10 @@ class TableHeader extends React.Component {
             <div className='header'>
                 <input className='header__select col col1' type='checkbox'></input>
                 <span className='header__status col col2'>Status</span>
-                <p className='header__name col col3'>Name</p>
-                <p className='header__type col col4'>Type</p>
-                <p className='header__tags col col5'>Tags</p>
-                <p className='header__owner col col6'>Owner</p>
+                <p className='header__name table__col col3'>Name</p>
+                <p className='header__type table__col col4'>Type</p>
+                <p className='header__tags table__col col5'>Tags</p>
+                <p className='header__owner table__col col6'>Owner</p>
             </div>
         )
     }
--- src/components/table/row/row.js
+++ src/components/table/row/row.js
@@ -25,12 +25,12 @@ class Row extends React.Component {
                 {this.state.rows.map(row => {
                     return (
                         <div className='rows__row' key={row.id}>
-                            <input className='rows__row--select col col1' type='checkbox'></input>
-                            <span className='col col2'><p className={`rows__row--status ${row.status}`}>{row.status}</p></span>
-                            <p className='rows__row--name col col3'>{row.name}</p>
-                            <p className='rows__row--type col col4'>{row.type}</p>
-                            <p className='rows__row--tags col col5'>{row.tags}</p>
-                            <p className='rows__row--owner col col6'>{row.owner}</p>
+                            <input className='rows__row--select table__col col1' type='checkbox'></input>
+                            <span className='table__col col2'><p className={`rows__row--status ${(row.status).toLowerCase()}`}>{row.status}</p></span>
+                            <p className='rows__row--name table__col col3'>{row.name}</p>
+                            <p className='rows__row--type table__col col4'>{row.type}</p>
+                            <p className='rows__row--tags table__col col5'>{row.tags}</p>
+                            <p className='rows__row--owner table__col col6'>{row.owner}</p>
                         </div>
                     )
                 })}
