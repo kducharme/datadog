d8123b6c0f46018794672f606cfbfb314c1e1ee7
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-05-31 19:55:52 -0500

    fixed box layout

--- src/components/navigation/page/pageNav.js
+++ src/components/navigation/page/pageNav.js
@@ -4,26 +4,12 @@ import { Link } from "react-router-dom";
 import AddRow from '../../../pages/addRow';
 
 class PageNavigation extends React.Component {
-    
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //         active: menuItem
-    //     }
-    // }
-
-
-    activePage(e) {
-        e.preventDefault;
-    }
-    
-    
     render() {
         return (
             <div className='pageNav'>
                 <div className="pageNav__links">
-                    <Link className={`pageNav__links--link ${window.location.pathname === '/' ? 'active': null }`} onClick={e => this.activePage(e)} to="/">Monitors</Link>
-                    <Link className={`pageNav__links--link ${window.location.pathname === '/downtime' ? 'active': null }`} onClick={e => this.activePage.bind(this)} to="/downtime">Downtime</Link>
+                    <Link className={`pageNav__links--link ${window.location.pathname === '/' ? 'active': null }`} to="/">Monitors</Link>
+                    <Link className={`pageNav__links--link ${window.location.pathname === '/downtime' ? 'active': null }`} to="/downtime">Downtime</Link>
                 </div>
                 <div className="pageNav__add">
                     <AddRow />
